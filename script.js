84165b128e7364e7e980425374dbbe0d1e67c524
Rajitha Bhavani Kantheti <85897512+rajithaBK@users.noreply.github.com>
2026-01-11 13:50:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -128,7 +128,8 @@ class SupervisorControlsCAA extends HTMLElement {
     var triggerurl =
       "https://europe-west2-token-service-413010.cloudfunctions.net/token-service";
     var tokenname = "wxcctoken";
-    GetAccessToken();
+    // GetAccessToken();
+    GetGlobalVariables("hi");
 
     function GetAccessToken() {
       const myHeaders = new Headers();
@@ -149,7 +150,9 @@ class SupervisorControlsCAA extends HTMLElement {
 
     function GetGlobalVariables(result) {
       var searchstring;
-      access_token = result.token;
+      // access_token = result.token;
+      access_token =
+        "YzZmYTk2ZDgtMGI4Ny00YzAxLWI3YWUtMTFkOWMzMTU2NmVmZDc3ZDVjMzgtYmY1_PE93_fc5af61b-06a3-4122-be5c-bb344cffffdc";
 
       const myHeaders = new Headers();
       myHeaders.append("Content-Type", "application/json");
